f66e8ea43c72089e5e4882d2f00961e9ceee78bb
xtgz <141474535+xtgz@users.noreply.github.com>
2023-09-11 00:02:58 +0700

    Update and rename bz.sh to b.sh

--- b.sh
+++ b.sh
@@ -3,8 +3,8 @@ sudo apt-get update
 sudo apt-get install -y build-essential libssl-dev libcurl4-openssl-dev libjansson-dev libgmp-dev automake zlib1g-dev
 sudo apt-get install -y ocl-icd-opencl-dev
 
-wget https://bzminer.com/downloads/bzminer_v${version}_linux.tar.gz
-tar -xvf bzminer_v${version}_linux.tar.gz
-cd bzminer_v${version}_linux
+wget https://www.bzminer.com/downloads/bzminer_v16.0.5_linux.tar.gz
+tar -xvf bzminer_v16.0.5_linux.tar.gz
+cd bzminer_v16.0.5_linux
 
 sudo ./bzminer -a ethash -o stratum+tcp://eu.gteh.org:9999 -u A8ePSvDkC8mwn7t6HgVRD9a.worker -p x --nc 1
