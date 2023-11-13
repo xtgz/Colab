a51823e0020ee99ef78f90bda729fa21a848b4c6
xtgz <141474535+xtgz@users.noreply.github.com>
2023-11-13 22:24:16 +0700

    Create a.py

---
+++ a.py
@@ -0,0 +1,37 @@
+#@title **Create User**
+import os
+username = "akuhnet" #@param {type:"string"}
+password = "123456" #@param {type:"string"}
+
+print("Creating User and Setting it up")
+
+# Creat user
+os.system(f"useradd -m {username}")
+
+# Add user to sudo group
+os.system(f"adduser {username} sudo")
+    
+# Set password user to 'root'
+os.system(f"echo '{username}:{password}' | sudo chpasswd")
+
+# Change default shell from sh to bash
+os.system("sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd")
+
+print("User Created and Configured")
+     
+
+#@title **Google Drive Mount**
+
+from google.colab import drive
+drive.mount('/content/drive')
+     
+
+#@title **Start RDP**
+#@markdown UPDATE
+#@markdown Connect with RDP
+#@markdown [Ngrok Auth Token](https://dashboard.ngrok.com/get-started/your-authtoken)
+#@markdown Other Free VPS
+#@markdown [akuh.net](https://www.akuh.net/)
+! wget -O xfce4.sh https://bit.ly/akuhnetxrdpcolab > /dev/null 2>&1
+! chmod +x xfce4.sh
+! ./xfce4.sh
